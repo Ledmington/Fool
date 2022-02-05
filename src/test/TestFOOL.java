1943819a3435eef8b2a2befe836dbdf1cf8e1303
Ledmington <ledmington.dev@gmail.com>
2022-02-05 09:46:18 +0100

    Added TestFOOL

---
+++ src/test/TestFOOL.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestFOOL {
+
+	@Test
+	public void simple() {
+		assertTrue(true);
+	}
+}
